c936ace1db119b504c94816350f639462b7f360e
Tim Jacomb <timjacomb1@gmail.com>
2023-11-28 12:08:20 +0000

    Sync issue contents from Jira to GitHub

---
+++ jira-automation/sync-contents.mjs
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+import {findIssueNumberFromJiraKey, updateGitHubIssue} from "./github.mjs";
+import {getIssue} from "./jira.mjs";
+import { assertCredentialsPresent } from "./utils.mjs";
+
+import {jiraToGitHub} from "./jira-to-github-processing.mjs";
+
+const issueKey = process.env.JIRA_ISSUE_KEY
+
+if (!issueKey) {
+    console.error('JIRA_ISSUE_KEY environment variable not set')
+    process.exit(1)
+}
+
+assertCredentialsPresent()
+
+async function processIssues() {
+    const { issueId } = await findIssueNumberFromJiraKey({issueKey})
+
+    let issue
+    try {
+        issue = await getIssue({key: issueKey})
+    } catch (err) {
+        console.log("Error searching for issue in jira", issueKey, err)
+
+        process.exit(1)
+    }
+
+    console.log("Processing issue", issue.key, issue.fields.summary)
+
+    const converted = jiraToGitHub({
+        issueId: issue.key,
+        content: issue.fields.description
+    })
+
+    console.log(converted)
+
+    await updateGitHubIssue({
+        issueId,
+        title: issue.fields.summary,
+        body: converted
+    })
+}
+
+await processIssues()
